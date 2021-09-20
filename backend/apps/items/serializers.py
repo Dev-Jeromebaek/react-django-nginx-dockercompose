5a6118ac624c44bc5b424f9946c0282e87418fd2
seungyeob <superior051@icloud.com>
2021-09-20 14:20:09 +0900

    chore: Completed template configuration

--- backend/apps/items/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Item
-        # fields = ('id', 'name', 'content', 'created_at', 'updated_at',)
-        fields = '__all__'
--- backend/apps/items/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from rest_framework.routers import SimpleRouter
-
-router = SimpleRouter()
-router.register(r'items', views.ItemViewSet)
-
-urlpatterns = router.urls
